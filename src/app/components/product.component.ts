da04fbf66b4469466606420d1c9bd42d4dd95941
Rafael Soriano Ramírez <rafael.soriano@arteenarte.com>
2020-06-14 23:21:28 -0500

    componentes, product y cart, estilos scss

---
+++ src/app/components/product.component.ts
@@ -0,0 +1,47 @@
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+  OnInit,
+  OnDestroy
+} from '@angular/core';
+
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+
+export class ProductComponent implements OnChanges, OnInit, OnDestroy {
+
+  @Input() product: Product;
+  @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+  constructor() {
+    console.log('1. constructor');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('2. ngOnChange');
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log('3. ngOnInit');
+  }
+
+  ngOnDestroy() {
+    console.log('4. ngOnDestroy');
+  }
+
+  addCart() {
+    console.log('Añadir al carrito');
+    this.productClicked.emit(this.product.id);
+  }
+
+}

08d0cb5f033e417aaf1fb6e663be283860fa4bee
Rafael Soriano Ramírez <rafael.soriano@arteenarte.com>
2020-06-23 00:51:36 -0500

    Delete y update de productos

--- src/app/product/components/product-detail/product-detail.component.ts
+++ src/app/product/components/product-detail/product-detail.component.ts
@@ -45,4 +45,22 @@ export class ProductDetailComponent implements OnInit {
       console.log(product);
     });
   }
+
+  updateProduct() {
+    const updateProduct: Partial<Product> = {
+      price: 7778889,
+      description: 'Edición título'
+    };
+    this.productsService.updateProduct('7689098', updateProduct)
+    .subscribe( product => {
+      console.log(product);
+    });
+  }
+
+  deleteProduct() {
+    this.productsService.deleteProduct('7689098')
+    .subscribe( rta => {
+      console.log(rta);
+    });
+  }
 }

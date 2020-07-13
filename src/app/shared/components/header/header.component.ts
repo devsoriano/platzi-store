c0cae3c9dcf77e9403cc0af4bb684f4cd20ac25b
Rafael Soriano Ramírez <rafael.soriano@arteenarte.com>
2020-07-13 16:57:57 -0500

    observable async para carrito

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { CartService } from './../../../core/services/cart.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -9,16 +10,13 @@ import { CartService } from './../../../core/services/cart.service';
 })
 export class HeaderComponent implements OnInit {
 
-  total = 0;
+  total$: Observable<number>;
 
   constructor(
     private cartService: CartService
   ) {
-    this.cartService.cart$
-    .pipe(map(products => products.length))
-    .subscribe(total => {
-      this.total = total;
-    });
+    this.total$ = this.cartService.cart$
+    .pipe(map(products => products.length));
    }
 
   ngOnInit(): void {

101c1ed26a3b8cd8960f3f194851a3e217429d90
Rafael Soriano Ramírez <rafael.soriano@arteenarte.com>
2020-07-13 23:02:07 -0500

    subir imágenes desde administrador

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { ProductsService } from 'src/app/core/services/products/products.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { MyValidators } from 'src/app/utils/validators';
+import { AngularFireStorage } from '@angular/fire/storage';
+
+import { finalize } from 'rxjs/operators';
+
+import { MyValidators } from './../../../utils/validators';
+
+import { ProductsService } from './../../../core/services/products/products.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form-product',
@@ -12,28 +18,49 @@ import { MyValidators } from 'src/app/utils/validators';
 export class FormProductComponent implements OnInit {
 
   form: FormGroup;
+  image$: Observable<any>;
 
   constructor(
     private formBuilder: FormBuilder,
-    private productServices: ProductsService,
-    private router: Router
+    private productsService: ProductsService,
+    private router: Router,
+    private storage: AngularFireStorage
   ) {
     this.buildForm();
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
   saveProduct(event: Event) {
     event.preventDefault();
     if (this.form.valid) {
       const product = this.form.value;
-      this.productServices.createProduct(product)
-      .subscribe( newProduct => {
+      this.productsService.createProduct(product)
+      .subscribe((newProduct) => {
         console.log(newProduct);
         this.router.navigate(['./admin/products']);
       });
     }
+  }
+
+  uploadFile(event) {
+    const file = event.target.files[0];
+    const name = 'image.png';
+    const fileRef = this.storage.ref(name);
+    const task = this.storage.upload(name, file);
+
+    task.snapshotChanges()
+    .pipe(
+      finalize(() => {
+        this.image$ = fileRef.getDownloadURL();
+        this.image$.subscribe(url => {
+          console.log(url);
+          this.form.get('image').setValue(url);
+        });
+      })
+    )
+    .subscribe();
   }
 
   private buildForm() {
@@ -42,11 +69,12 @@ export class FormProductComponent implements OnInit {
       title: ['', [Validators.required]],
       price: ['', [Validators.required, MyValidators.isPriceValid]],
       image: [''],
-      description: ['', [Validators.required]]
+      description: ['', [Validators.required]],
     });
   }
 
   get priceField() {
     return this.form.get('price');
   }
+
 }

a4fdecfca40d3ea01d4fde98caf88cbaf61a65f5
Rafael Soriano Ramírez <rafael.soriano@arteenarte.com>
2020-07-13 22:01:42 -0500

    implementar firebase y módulo de registro cuenta de correo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,12 @@ import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+import { environment } from './../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +28,10 @@ import { HttpClientModule } from '@angular/common/http';
     SharedModule,
     CoreModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private afa: AngularFireAuth
+  ) { }
+
+  createUser(email: string, password: string) {
+    return this.afa.auth.createUserWithEmailAndPassword(email, password);
+  }
+}

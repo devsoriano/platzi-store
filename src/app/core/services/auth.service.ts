ef6e4b193f7925fc3437545c4176a19e2fbaf4cf
Rafael Soriano Ramírez <rafael.soriano@arteenarte.com>
2020-07-13 22:32:33 -0500

    login y guardianes para autenticación

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -7,10 +7,22 @@ import { AngularFireAuth } from '@angular/fire/auth';
 export class AuthService {
 
   constructor(
-    private afa: AngularFireAuth
+    private af: AngularFireAuth
   ) { }
 
   createUser(email: string, password: string) {
-    return this.afa.auth.createUserWithEmailAndPassword(email, password);
+    return this.af.auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  login(email: string, password: string) {
+    return this.af.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  logout() {
+    return this.af.auth.signOut();
+  }
+
+  hasUser() {
+    return this.af.authState;
   }
 }
